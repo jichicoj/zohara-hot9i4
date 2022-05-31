74952290c77c6f396d1f2a3a2728cbd80ce6c95a
Joshua Isaí Chicoj <81249233+jichicoj@users.noreply.github.com>
2022-05-31 02:52:20 -0600

    URL de API actualizado

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,5 +1,5 @@
 export const GLOBAL = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  URL: 'http://localhost:5000/api/'
-  //URL: 'https://zohara.herokuapp.com/api/'
+  //URL: 'http://localhost:5000/api/'
+  URL: 'https://zohara.herokuapp.com/api/'
 };
